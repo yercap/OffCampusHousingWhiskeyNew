11c793156e08587b810a485827a89fc9f8f1a35a
burnworc <70215302+burnworc@users.noreply.github.com>
2020-10-19 22:45:16 -0400

    Update Agent.java

--- WhiskeyOffCampusHousing/src/Agent.java
+++ WhiskeyOffCampusHousing/src/Agent.java
@@ -17,19 +17,19 @@ public class Agent {
 	}
 	
 	public void editProperty(int propertyID, String title) {
-		myProperties.set(propertyID, title);
+		myProperties.add(propertyID, title);
 	}
 	
 	public void editPrice(int propertyID, double price) {
-		myProperties.set(propertyID, price);
+		myProperties.add(propertyID, price);
 	}
 	
 	public void editDescription(int propertyID, ArrayList<String> Description) {
-		myProperties.set(propertyID, Description);
+		myProperties.add(propertyID, Description);
 	}
 	
 	public void editBedrooms(int propertyID, int bedrooms) {
-		myProperties.set(propertyID, bedrooms);
+		myProperties.add(propertyID, bedrooms);
 	}
 	
 	public void editBathrooms(int propertyID, double bathrooms) {
@@ -37,10 +37,10 @@ public class Agent {
 	}
 	
 	public void editRestriction(int propertyID, ArrayList<Restriction> Restriction) {
-		myProperties.set(propertyID, Restriction);
+		myProperties.add(propertyID, Restriction);
 	}
 	
 	public void editSimilarProperty(int propertyID, ArrayList<Property> similarProperties) {
-		myProperties.set(propertyID, similarProperties);
+		myProperties.add(propertyID, similarProperties);
 	}
 }
