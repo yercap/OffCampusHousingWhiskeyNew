78ef132a324ea5cd0d6c195df43ddbffca3eb80f
yerald <yerald@Yerald-G3-3579>
2020-10-27 02:03:09 -0400

    Adding data constants for jsonparser

---
+++ WhiskeyOffCampusHousing/src/DataConstants.java
@@ -0,0 +1,28 @@
+
+public abstract class DataConstants {
+	protected static final String ACCOUNT_FILE = "JSON files/account.json";
+	protected static final String ACCOUNT_FIRST_NAME = "firstName";
+	protected static final String ACCOUNT_LAST_NAME = "lastName";
+	protected static final String ACCOUNT_USERNAME = "userName";
+	protected static final String ACCOUNT_ID = "uscID";
+	protected static final String ACCOUNT_EMAIL = "email";
+	protected static final String ACCOUNT_FAVORITE_PROPERTIES = "favoriteProperties";
+	protected static final String ACCOUNT_TYPE = "type";
+	
+	protected static final String PROPERTY_FILE = "JSON files/property.json";
+	protected static final String PROPERTY_TITLE = "title";
+	protected static final String PROPERTY_ADDRESS = "address";
+	protected static final String PROPERTY_BEDROOMS = "bedrooms";
+	protected static final String PROPERTY_DWELLING_TYPE = "dwellingType";
+	protected static final String PROPERTY_PRICE = "price";
+	protected static final String PROPERTY_AGENT = "agent";
+	protected static final String PROPERTY_DIST = "distanceToCampus";
+	protected static final String PROPERTY_RESTRICTION = "restriction";
+	protected static final String PROPERTY_DESCRIPTION = "description";
+	protected static final String PROPERTY_SIMILAR_PROPERTIES = "similarProperties";
+	protected static final String PROPERTY_REVIEWS = "reviews";
+	
+	protected static final String REIVEW_WRITER = "writer";
+	protected static final String REIVEW_RATING = "rating";
+	protected static final String REIVEW_FEEDBACK = "feedback";
+}
