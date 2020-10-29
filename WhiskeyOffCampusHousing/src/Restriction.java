b3c39c6372a52255ad98a062187bce7959b1d0a0
Jerald Capilo <yerald@Yerald-G3-3579>
2020-10-29 03:00:39 -0400

    Creating restriction stub

---
+++ WhiskeyOffCampusHousing/src/Restriction.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Jerald Capilo
+ *
+ */
+public class Restriction {
+	String restriction;
+	
+	public Restriction(String restriction) {
+		this.restriction = restriction;
+	}
+	
+	public void setRestriction(String restriction) {
+		this.restriction = restriction;
+	}
+
+	public String getRestriction() {
+		return restriction;
+	}
+}
